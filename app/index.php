630877d76134c6eec68352ef732b65744c394171
Marcelo Brito <marcelogbrito@gmail.com>
2020-03-30 18:34:02 -0300

    correção do select que chamava onome tabela errada

--- app/index.php
+++ app/index.php
@@ -4,14 +4,17 @@ $dbuser = $_ENV['MYSQL_USER'];
 $dbpass = $_ENV['MYSQL_PASS'];
 $dadosXls = "";
 try {
-    $pdo = new PDO("mysql:host=mysql:3307;dbname=corona_db", $dbuser, $dbpass);
-    $statement = $pdo->prepare("SELECT * FROM corona_db.tbl_voluntarios");
+    $pdo = new PDO("mysql:host=mysql;dbname=corona_db", $dbuser, $dbpass);
+    $statement = $pdo->prepare("SELECT * FROM corona_db.tbl_voluntariados");
     $statement->execute();
     $voluntarios = $statement->fetchAll(PDO::FETCH_OBJ);
+    //var_dump($voluntarios);
     } catch(PDOException $e) {
     echo $e->getMessage();
 }
-    $dadosXls .= "<table><thead><tr><th>id</th><th>nome</th><th>ocupacao</th><th>subcategoria</th><th>telefone</th><th>celular</th><th>email</th><th>cpf</th><th>registro profisisonal</th><th>ano</th><th>periodo</th></tr></thead><tbody>";
+    $dadosXls .= "<table><thead><tr><th>id</th><th>nome</th><th>ocupacao</th><th>subcategoria</th>
+    <th>telefone</th><th>celular</th><th>email</th><th>cpf</th><th>registro profisisonal</th>
+    <th>ano</th><th>periodo</th></tr></thead><tbody>";
     foreach ($voluntarios as $voluntario ) {
         $dadosXls .= "<tr><td>".$voluntario->id."</td><td>".$voluntario->nome."</td><td>".$voluntario->ocupacao."</td><td>".$voluntario->subcategoria."</td><td>".$voluntario->telefone."</td><td>".$voluntario->celular."</td><td>".$voluntario->email."</td><td>".$voluntario->cpf."</td><td>".$voluntario->registro_profissional."</td><td>".$voluntario->ano."</td><td>".$voluntario->periodo."</td></tr>";
     }
